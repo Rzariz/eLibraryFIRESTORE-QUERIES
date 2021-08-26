75c3c50298370353e94d397fd48d1172477bca94
varsha <varsha@Rza-MacBook-Pro.local>
2021-08-26 19:55:39 +0530

    Autofill & Bottom tab

--- screens/Transaction.js
+++ screens/Transaction.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, TextInput, ImageBackground, Image } from "react-native";
 
 import * as Permissions from "expo-permissions";
 import { BarCodeScanner } from "expo-barcode-scanner";
+
+const bgImage = require("../assets/background2.png");
+const appIcon = require("../assets/appIcon.png");
+const appName = require("../assets/appName.png");
 
 export default class TransactionScreen extends Component {
   constructor(props) {
@@ -11,7 +15,9 @@ export default class TransactionScreen extends Component {
       domState: "normal",
       hasCameraPermissions: null,
       scanned: false,
-      scannedData: ""
+      scannedData: "",
+      bookId: "",
+      studentId: "",
     }
   }
 
@@ -28,16 +34,31 @@ export default class TransactionScreen extends Component {
   };
 
   handleBarCodeScanned = async ({ type, data }) => {
-    this.setState({
-      scannedData: data,
-      domState: "normal",
-      scanned: true
-    });
+    const { domState } = this.state;
+    if (domState === "bookId") {
+      this.setState({
+        bookId: data,
+        domState: "normal",
+        scanned: true
+      });
+    } else if (domState === "studentId") {
+      this.setState({
+        studentId: data,
+        domState: "normal",
+        scanned: true
+      });
+    }
+
+    // this.setState({
+    //   scannedData: data,
+    //   domState: "normal",
+    //   scanned: true
+    // });
   };
 
   render() {
-    const { domState, hasCameraPermissions, scannedData, scanned } = this.state;
-    if (domState === "scanner") {
+    const { domState, bookId, studentId, scanned } = this.state;
+    if (domState !== "normal") {
       return (
         <BarCodeScanner
           onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
@@ -47,14 +68,55 @@ export default class TransactionScreen extends Component {
     }
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>
+
+        <ImageBackground source={bgImage} style={styles.bgImage}>
+          <View style={styles.upperContainer}>
+            <Image source={appIcon} style={styles.appIcon} />
+            <Image source={appName} style={styles.appName} />
+          </View>
+
+
+          <View style={styles.lowerContainer}>
+
+
+            <View style={styles.textinputContainer}>
+              <TextInput
+                style={styles.textinput}
+                placeholder={"Book Id"}
+                placeholderTextColor={"#FFFFFF"}
+                value={bookId} />
+              <TouchableOpacity
+                style={styles.scanbutton}
+                onPress={() => this.getCameraPermissions("bookId")} >
+                <Text style={styles.scanbuttonText}>Scan</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={[styles.textinputContainer, { marginTop: 25 }]}>
+              <TextInput
+                style={styles.textinput}
+                placeholder={"Student Id"}
+                placeholderTextColor={"#FFFFFF"}
+                value={studentId}
+              />
+              <TouchableOpacity
+                style={styles.scanbutton}
+                onPress={() => this.getCameraPermissions("studentId")}
+              >
+                <Text style={styles.scanbuttonText}>Scan</Text>
+              </TouchableOpacity>
+            </View>
+
+          </View>
+          {/* <Text style={styles.text}>
           {hasCameraPermissions ? scannedData : "Request for Camera Permission"}
         </Text>
         <TouchableOpacity
           style={styles.button}
           onPress={() => this.getCameraPermissions("scanner")}>
           <Text style={styles.buttonText}>Scan QR Code</Text>
-        </TouchableOpacity>
+        </TouchableOpacity> */}
+        </ImageBackground>
       </View>
     );
   }
@@ -82,5 +144,64 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 24,
     color: "#FFFFFF"
-  }
+  },
+  lowerContainer: {
+    flex: 0.5,
+    alignItems: "center"
+  },
+  textinputContainer: {
+    borderWidth: 2,
+    borderRadius: 10,
+    flexDirection: "row",
+    backgroundColor: "#9DFD24",
+    borderColor: "#FFFFFF"
+  },
+  textinput: {
+    width: "57%",
+    height: 50,
+    padding: 10,
+    borderColor: "#FFFFFF",
+    borderRadius: 10,
+    borderWidth: 3,
+    fontSize: 18,
+    backgroundColor: "#5653D4",
+    fontFamily: "Rajdhani_600SemiBold",
+    color: "#FFFFFF"
+  },
+  scanbutton: {
+    width: 100,
+    height: 50,
+    backgroundColor: "#9DFD24",
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  scanbuttonText: {
+    fontSize: 24,
+    color: "#0A0101",
+    fontFamily: "Rajdhani_600SemiBold"
+  },
+  bgImage: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
+  },
+  upperContainer: {
+    flex: 0.5,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  appIcon: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+    marginTop: 80
+  },
+  appName: {
+    width: 80,
+    height: 80,
+    resizeMode: "contain"
+  },
+
 });
